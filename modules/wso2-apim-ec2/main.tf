209b1b25e79332bfff78c0f508c202d4c87b8fb0
Renuka Fernando <renukapiyumal@gmail.com>
2019-10-31 16:48:43 +0530

    add security group and install docker on ec2 using ssh and remote-exec provisioner

--- modules/wso2-apim-ec2/main.tf
+++ modules/wso2-apim-ec2/main.tf
@@ -24,11 +24,67 @@ resource "aws_key_pair" "this" {
   public_key = file("./resources/ssh-key/wso2-apim-aws.pub")
 }
 
+resource "aws_security_group" "this" {
+  name = "SETF-wso2-apim-security-group"
+  description = "Default security group that allows inbound and outbound traffic from all instances in the VPC"
+
+  ingress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    self        = true
+  }
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    self        = true
+  }
+  egress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SETF-WSO2-APIM-Security-Group"
+  }
+}
+
 resource "aws_instance" "this" {
   ami = data.aws_ami.amazon_ami.id
   instance_type = "t3a.medium"
 
   key_name = aws_key_pair.this.key_name
 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo amazon-linux-extras install -y docker",
+      "sudo service docker start",
+      "sudo usermod -a -G docker ec2-user"
+    ]
 
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      host = aws_instance.this.public_dns
+      private_key = file("resources/ssh-key/wso2-apim-aws")
+    }
+  }
+
+  tags = {
+    Name = "SETF-WSO2-APIM-Security-Group"
+  }
 }
